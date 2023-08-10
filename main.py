06701b19fdca4d48f16fd3410353f8deb96b8da8
Amirlashkar <amireza1422@gmail.com>
2023-08-11 02:26:14 +0330

    1st commenting

--- main.py
+++ main.py
@@ -11,26 +11,54 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras import Sequential
 from tensorflow import keras
 
+# Main Object
 class AnomalyDetector:
 
     def __init__(self):
+        # reading yml file for constant values calling (whenever you wanted to change some variable 
+        # its just easier to change it on config.yml)
         with open('config.yml', 'r') as f:
                 config = yaml.safe_load(f)["variables"]
+        # making cofig main feature of object cause its needed to be called on other subclasses also
         self.config = config
 
     class PreProcessPipeline:
+        """
+        description:
+        PreProcess Pipeline is needed either for Training and Predicting ; but there is just slight change and it can be deployed
+        by changing stage argument on __call__ function
+        """
 
         def __init__(self) -> None:
+            # calling config dictionary and variables from it
             config = AnomalyDetector().config
             self.mean_chunk_size = config["mean_chunk_size"]
-            self.dataset_timestamps = config["dataset_timestamps"]
+            self.window_size = config["window_size"]
 
         def __call__(self, timeseries_df, stage) -> tuple:
+            """
+            description:
+            whole implementation of pipeline and components are created on __call__ function of each Pipeline to make class act as a function (
+            we give it an input and expect an specific output)
+
+            args:
+            timeseries_df -> a dataframe with columns like timestamp, value, and label
+            stage -> which stage you want this pipeline for? train or pred
+            """
             df = self.double_variance(timeseries_df, stage)
             data_tuple = self.dataset(df)
             return data_tuple
         
         def mean_chunk(self, seq, size) -> list:
+            """
+            description:
+            gets a series of data like python list or pandas Series as input and chunks it to same sizes ;
+            measures mean of every chunk and place it as main value with same order of main data value
+
+            args:
+            seq -> sequence of data
+            size -> size of chunks
+            """
             num = len(seq) // size
             avg = len(seq) / float(num)
             out = []
@@ -47,12 +75,23 @@ class AnomalyDetector:
 
             return replicated
 
-        def windowing(self, X, Y, timesteps=1) -> np.ndarray:
+        def windowing(self, X, Y, window_size=1) -> np.ndarray:
+            """
+            description:
+            implements sliding window on data, every window differs just one value from previous window thus every (n, ) shape data would
+            be converted to (n, window_size, 1) shape data
+
+            args:
+            X -> data with model input shape
+            Y -> data with model output shape
+            with respect to AutoEncoder Type model, both X and Y have same shape to each other
+            window_size -> obvious
+            """
             Xs, Ys = [], []
-            for i in range(len(X) - timesteps):
-                v = X.iloc[i:(i + timesteps)].value
+            for i in range(len(X) - window_size):
+                v = X.iloc[i:(i + window_size)].value
                 Xs.append(v)
-                Ys.append(Y.iloc[i + timesteps])
+                Ys.append(Y.iloc[i + window_size])
             return np.array(Xs), np.array(Ys)
 
         def double_variance(self, df, stage) -> pd.DataFrame:
@@ -61,12 +100,19 @@ class AnomalyDetector:
             with desired feature which is called double variance ; double variance is good enough for magnifying
             samples away from partial mean or that chunk mean.
             """
+            # measuring partial mean
             df["mean"] = self.mean_chunk(df["value"], self.mean_chunk_size)
+            # measuring partial variance
             df["variance"] = (df["value"] - df["mean"]) ** 2
+            # repeating last process on variance
             df["variance_mean"] = self.mean_chunk(df["variance"], self.mean_chunk_size)
             df["double_variance"] = (df["variance"] - df["variance_mean"]) ** 2
+            # removing unwanted columns
             df.drop(["mean", "value", "variance", "variance_mean"], axis=1, inplace=True)
+            # renaming double_variance to value (main data for model is double_variance)
             df = df.rename(columns={"double_variance": "value"})
+            # we'll normalize data for training if stage is declared as train ; cause due to decided architecture for this problem solution
+            # every anomalous data data should be brought to normal distribution ; so we change double_variance to 0
             if stage == "train":
                 df.loc[df["label"] == 1, "value"] = 0
             return df
@@ -79,7 +125,7 @@ class AnomalyDetector:
             # test["value"] = pca.fit_transform(test[["value"]])
             # train["value"] = scaler.fit_transform(train[["value"]])
             # test["value"] = scaler.fit_transform(test[["value"]])
-            X_data, Y_data = self.windowing(data[["value"]], data.value, self.dataset_timestamps)
+            X_data, Y_data = self.windowing(data[["value"]], data.value, self.window_size)
             X_data = X_data.reshape(X_data.shape[0], X_data.shape[1], 1)
             return (X_data, Y_data)
             
@@ -150,6 +196,7 @@ class AnomalyDetector:
         def __init__(self):
             config = AnomalyDetector().config
             self.threshold = config["threshold"]
+            self.window_size = config["window_size"]
             if self.threshold == None:
                 raise ValueError("Threshold value is not valid and is None by now ; train a model with \
                                  'AnomalyDetector.TrainingPipeline' to change it")
@@ -175,11 +222,11 @@ class AnomalyDetector:
             X_test, _ = data_tuple
             X_test_pred = self.model.predict(X_test)
             test_mae_loss = np.mean(np.abs(X_test_pred - X_test), axis=1)
-            test_score_df = pd.DataFrame(index=timeseries[self.dataset_timestamps:].index)
+            test_score_df = pd.DataFrame(index=timeseries[self.window_size:].index)
             test_score_df['loss'] = test_mae_loss
             test_score_df['threshold'] = self.threshold
             test_score_df['above_anomaly'] = test_score_df.loss > test_score_df.threshold
-            test_score_df['value'] = timeseries[self.dataset_timestamps:].value
+            test_score_df['value'] = timeseries[self.window_size:].value
             return test_score_df
         
         def anomaly_detection(self, test_score_df) -> Any:
